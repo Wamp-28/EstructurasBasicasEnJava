edd86626b6508e059fa124d583466f294e39e31f
AlexM <wmatallana28@gmail.com>
2025-02-20 12:34:13 -0500

    Variables

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,50 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Variables ENTERAS
+
+        byte numero1= 120;
+
+        System.out.println("VARIABLE DE TIPO BYTE: " + numero1);
+
+        short numero2= 30400;
+        System.out.println("VARIABLE DE TIPO SHORT: " + numero2);
+
+        int numero3 = 235;
+        System.out.println("VARIABLE DE TIPO INT: " + numero3);
+
+        long numero4= 5623L;
+
+        System.out.println("VARIABLE DE TIPO LONG: " + numero4);
+
+
+        // VARIABLES DECIMALES
+
+        float decimal1 = 32.5f;
+        System.out.println("VARIABLE DE TIPO FLOAT: " + decimal1);
+
+        double decimal2 = 125.6;
+        System.out.println("VARIABLE DE TIPO DOUBLE: " + decimal1);
+
+        // VARIABLES DE TIPO CADENA DE CARACTER
+
+        String nombre= "William Alexander Matallana";
+        System.out.println("VARIABLE DE TIPO CADENA DE CARACTER: " + nombre);
+
+        // VARIABLE DE TIPO CARACTER
+
+        char letra = 'a';
+        char letra1 = 65;
+
+        System.out.println("VARIABLE DE TIPO CARACTER: " + letra);
+        System.out.println("VARIABLE DE TIPO CARACTER: " + letra1);
+
+        // VARIABLE DE TIPO BOOLEAN
+
+        boolean estado = true;
+        System.out.println("VARIABLE DE TIPO BOOLEAN: " + estado);
+
+    }
+}
